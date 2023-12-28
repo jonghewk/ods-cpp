5d7f1bce71c175b37eab3c1905c0227299d8a26e
jonghewk <jonghewk.park@gmail.com>
2023-12-28 17:31:09 +0900

    working on rootish array stack

--- array_lists/rootish_array_stack.h
+++ array_lists/rootish_array_stack.h
@@ -1,11 +1,23 @@
 #pragma once
-
+#include <array_lists/array_stack.h>
 namespace ods {
 
 template <typename T> class RootishArrayStack {
 public:
+	int i2b(int i) {
+		double db = (-3.0 + std::sqrt(9 + 8 * i)) / 2.0;
+		int b = ceil(db);
+		return b;
+	}
 
+	T get(int i) {
+		int b = i2b(i);
+		int j = i - b * ( b + 1) / 2.0;
+		return blocks.get(b)[j];
+	}
 
+	ArrayStack<T*> blocks;
+	int n;
 };
 
 
